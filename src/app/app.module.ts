d0247111836d2675ac45c307a1c041954ed2bcd3
unknown <mustafahalici30@gmail.com>
2020-12-07 22:39:51 +0100

    ders1:component yaptik

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+  AppComponent,
+  MoviesComponent
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+    selector:"movies",
+    templateUrl:"movies.component.html"
+})
+
+export class MoviesComponent{
+
+}
